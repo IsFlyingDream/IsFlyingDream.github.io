4543df49abba88cabad880b661fc24b8b8a16ac6
IsFlyingDream <ruby29922026@gmail.com>
2021-06-03 16:39:51 +0800

    Update MyScript.js

--- css_js/MyScript.js
+++ css_js/MyScript.js
@@ -1,11 +1,14 @@
 
 //讓上方菜單frameid大小自動改變↓↓
-
-  var iframe = document.getElementById("ifrm");
+function doneLoading() {
+        var frame = document.getElementsByTagName('iframe')[0].contentWindow.document.body.innerHTML = "NOFIDDLE";
+    
+	var iframe = document.getElementById("ifrm");
 
         // Adjusting the iframe height onload event
         iframe.onload = function () {
             iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
         }
 
+}
 //讓上方菜單frameid大小自動改變↑↑

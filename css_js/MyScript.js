d75e4ad0623f1de153378ede3cf146eaaa540479
IsFlyingDream <ruby29922026@gmail.com>
2021-06-03 19:43:32 +0800

    Update MyScript.js

--- css_js/MyScript.js
+++ css_js/MyScript.js
@@ -8,7 +8,31 @@ function doneLoading() {
         // Adjusting the iframe height onload event
         iframe.onload = function () {
             iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+			
         }
 
 }
 //讓上方菜單frameid大小自動改變↑↑
+
+//上方菜單顯示與隱藏的按鈕↓↓
+
+function listBtn() {
+  var listBtn = document.getElementById('listBtn');
+  var textlistn = document.getElementById('HidAndShow');
+  if (textlistn.style.display ==="block") {
+    HidAndShow.style.display = "none";
+  } else {
+    HidAndShow.style.display = "block";
+  }
+}
+
+window.onresize=function(){  //每次視窗改變都會載入下列事件
+	if(document.body.scrollWidth >=  760){
+		 HidAndShow.style.display = "block";
+	}else{
+		HidAndShow.style.display = "none";
+	}
+    
+};
+
+//上方菜單顯示與隱藏的按鈕↑↑
